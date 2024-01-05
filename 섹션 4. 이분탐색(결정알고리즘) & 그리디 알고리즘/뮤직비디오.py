f797b71ce75eb12f830df003764b1b5257dc8b08
kimdongyoung <wwweric12@naver.com>
2024-01-05 18:26:42 +0900

    뮤직비디오 구현

---
+++ 섹션 4. 이분탐색(결정알고리즘) & 그리디 알고리즘/뮤직비디오.py
@@ -0,0 +1,73 @@
+n,m= map(int,input().split())
+lst=list(map(int,input().split()))
+
+lt=1
+rt=sum(lst)
+
+
+
+answer=0
+
+while lt<=rt:
+    mid= (lt+rt)//2
+    cnt=1
+    tmp=0
+    for i in lst:
+        tmp+=i
+        if tmp >mid :
+            cnt+=1
+            tmp=i 
+    if cnt <= m:
+        rt=mid-1
+        answer=mid
+    else:
+        lt=mid+1
+        
+        
+print(answer)
+        
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+# 답안
+
+
+# def Count(capacity):
+#     cnt=1
+#     sum=0
+#     for x in lst:
+#         if sum+x>capacity:
+#             cnt+=1
+#             sum=x
+#         else:
+#             sum+=x
+#     return cnt
+
+
+# n,m= map(int,input().split())
+# lst=list(map(int,input().split()))
+
+# lt=1
+# rt=sum(lst)
+# res =0
+
+# while lt<=rt:
+#     mid = (lt+rt)//2
+#     if Count(mid)<=m:
+#         res = mid
+#         rt=mid-1
+#     else:
+#         lt = mid+1
+
+# print(res)
