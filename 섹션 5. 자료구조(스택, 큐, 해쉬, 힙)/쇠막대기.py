6d4806409067065987dd9db63a5dadea84ee5543
kimdongyoung <wwweric12@naver.com>
2024-01-29 18:07:02 +0900

    fix:쇠막대기 답안 수정

--- 섹션 5. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
+++ 섹션 5. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
@@ -16,5 +16,18 @@ for i in range(len(lst)):
 print(cnt)
 
 
-#답안과 동일 
+#답안
 
+# s= input()
+# stack=[]
+# cnt=0
+# for i in range(len(s)):
+#     if s[i]== "(":
+#         stack.append(s[i])
+#     else:
+#         stack.pop()
+#         if s[i-1]=="(":
+#             cnt+=len(stack)
+#         else:
+#             cnt+=1
+# print(cnt)
