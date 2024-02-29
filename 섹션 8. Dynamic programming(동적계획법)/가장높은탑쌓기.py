bcc2a79b1c6a4899a51551514d34f35207c91bf4
kimdongyoung <wwweric12@naver.com>
2024-02-29 18:37:04 +0900

    fix: 백준 문제 구현

--- 섹션 8. Dynamic programming(동적계획법)/가장높은탑쌓기.py
+++ 섹션 8. Dynamic programming(동적계획법)/가장높은탑쌓기.py
@@ -1,36 +1,69 @@
-n=int(input())
+# n=int(input())
 
-lst =[list(map(int,input().split())) for _ in range(n)] 
-lst.sort(key= lambda x:x[2],reverse=True)
-dy=[0]*n
-dy[0]=lst[0][1]
-res=0
+# lst =[list(map(int,input().split())) for _ in range(n)] 
+# lst.sort(key= lambda x:x[2],reverse=True)
+# dy=[0]*n
+# dy[0]=lst[0][1]
+# res=0
 
-for i in range(1,n):
-    tmp=0
-    for j in range(i-1,-1,-1):
-        if lst[i][0] < lst[j][0] and dy[j] > tmp:
-            tmp = dy[j]
-    dy[i] = tmp + lst[i][1]
+# for i in range(1,n):
+#     tmp=0
+#     for j in range(i-1,-1,-1):
+#         if lst[i][0] < lst[j][0] and dy[j] > tmp:
+#             tmp = dy[j]
+#     dy[i] = tmp + lst[i][1]
 
-print(max(dy))
+# print(max(dy))
+
+    
 
 #답안 
 
-if __name__ == "__main__":
-    n= int(input())
-    bricks=[]
-    for i in range(n):
-        a , b, c = map(int,input().split())
-        bricks.append((a,b,c))
-    bricks.sort(reverse=True)
-    dy=[0]*n
-    dy[0]= bricks[0][1]
-    for i in range(1,n):
-        max_h=0
-        for j in range(i-1,-1,-1):
-            if bricks[j][2] > bricks[i][2] and dy[j] > max_h:
-                max_h=dy[j]
-        dy[i] = max_h + bricks[i][1]
-        res=max(res,dy[i])
-    print(res)
+# if __name__ == "__main__":
+#     n= int(input())
+#     bricks=[]
+#     for i in range(n):
+#         a , b, c = map(int,input().split())
+#         bricks.append((a,b,c))
+#     bricks.sort(reverse=True)
+#     dy=[0]*n
+#     dy[0]= bricks[0][1]
+#     for i in range(1,n):
+#         max_h=0
+#         for j in range(i-1,-1,-1):
+#             if bricks[j][2] > bricks[i][2] and dy[j] > max_h:
+#                 max_h=dy[j]
+#         dy[i] = max_h + bricks[i][1]
+#         res=max(res,dy[i])
+#     print(res)
+
+
+#백준
+
+n=int(input())
+lst =[list(map(int,input().split())) for _ in range(n)] 
+tlst=lst.copy()
+lst.sort(key= lambda x:x[2],reverse=True)
+dy=[0]*n
+k=[[lst[0][0]]]
+dy[0]=lst[0][1]
+
+
+for i in range(1,n):
+    tmp=0
+    s=[]
+    for j in range(i-1,-1,-1):
+        if lst[i][0] < lst[j][0] and dy[j] > tmp:
+            s = k[j]
+            tmp = dy[j]
+    dy[i] = tmp + lst[i][1]
+    k.append(s+[lst[i][0]])
+    
+c=k[dy.index(max(dy))]
+c.reverse()
+print(len(c))
+
+for a in c:
+    for b in range(len(tlst)):
+        if a == tlst[b][0]:
+            print(b+1)
