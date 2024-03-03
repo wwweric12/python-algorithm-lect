38fb2d07e12bdca515aea32c69be6d9eedaf43c9
kimdongyoung <wwweric12@naver.com>
2024-03-04 00:57:31 +0900

    fix: 회장뽑기 수정

--- 섹션 8. Dynamic programming(동적계획법)/회장뽑기.py
+++ 섹션 8. Dynamic programming(동적계획법)/회장뽑기.py
@@ -13,16 +13,19 @@ while True:
         dy[s-1][d-1]=1
         dy[d-1][s-1]=1
 
-for i in dy:
-    print(i)
-print()
 
-for k in range(n):
+
+for k in range(n): 
     for i in range(n):
         for j in range(n):
             if dy[i][j] == '' and dy[i][k] != ''  and dy[k][j] != '' :
                 dy[i][j] = dy[i][k]+dy[k][j]
                 dy[j][i] = dy[i][j]
+            elif dy[i][j] !='' and dy[i][k] != ''  and dy[k][j] != '' :
+               dy[i][j] =min(dy[i][j], dy[i][k]+dy[k][j])
+                
+    
+
     
 mv = 100
 lst =[]
@@ -46,46 +49,46 @@ for i in lst:
 
 #답안
 
-if __name__ == "__main__":
-    n= int(input())
-    dis=[[100]*(n+1) for _ in range(n+1)]
+# if __name__ == "__main__":
+#     n= int(input())
+#     dis=[[100]*(n+1) for _ in range(n+1)]
 
-    for i in range(1,n+1):
-        dis[i][i]=0
+#     for i in range(1,n+1):
+#         dis[i][i]=0
 
-    while True:
-        s,d  =map(int,input().split())
-        if s== -1 and d == -1:
-            break
-        else:
-            dis[s][d]=1
-            dis[d][s]=1
-    for x in dis:
-        print(x)
-    print()
+#     while True:
+#         s,d  =map(int,input().split())
+#         if s== -1 and d == -1:
+#             break
+#         else:
+#             dis[s][d]=1
+#             dis[d][s]=1
+#     for x in dis:
+#         print(x)
+#     print()
 
-    for k in range(1,n+1):
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                dis[i][j] = min(dis[i][j],dis[i][k]+dis[k][j])
+#     for k in range(1,n+1):
+#         for i in range(1,n+1):
+#             for j in range(1,n+1):
+#                 dis[i][j] = min(dis[i][j],dis[i][k]+dis[k][j])
     
-    for x in dis:
-        print(x)
+#     for x in dis:
+#         print(x)
                 
-    res=[0]*(n+1)
-    score = 100
+#     res=[0]*(n+1)
+#     score = 100
     
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            res[i]=max(res[i],dis[i][j])
-        score = min(score,res[i])
-    out =[]
-    for i in range(1,n+1):
-        if res[i]==score:
-            out.append(i)
-    print("%d %d" %(score,len(out)))
-    for x in out:
-        print(x,end=' ')            
+#     for i in range(1,n+1):
+#         for j in range(1,n+1):
+#             res[i]=max(res[i],dis[i][j])
+#         score = min(score,res[i])
+#     out =[]
+#     for i in range(1,n+1):
+#         if res[i]==score:
+#             out.append(i)
+#     print("%d %d" %(score,len(out)))
+#     for x in out:
+#         print(x,end=' ')            
             
             
 
