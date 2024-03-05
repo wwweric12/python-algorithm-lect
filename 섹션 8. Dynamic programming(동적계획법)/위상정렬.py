1b8a0a22ebc573e0c42865de057f89b7ee005dc1
kimdongyoung <wwweric12@naver.com>
2024-03-05 20:10:35 +0900

    위상정렬 구현

---
+++ 섹션 8. Dynamic programming(동적계획법)/위상정렬.py
@@ -0,0 +1,27 @@
+#답안 
+from collections import deque
+n , m = map(int,input().split())
+
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+degree = [0] *(n+1)
+dQ= deque()
+
+for _ in range(m):
+    s, d =map(int,input().split())
+    graph[s][d]=1
+    degree[d]+=1
+
+for i in range(1,n+1):
+    if degree[i]==0:
+        dQ.append(i)
+        
+while dQ:
+    x=dQ.popleft()
+    print(x, end=' ')
+    for i in range(1,n+1):
+        if graph[x][i]==1:
+            degree[i]-=1
+            if degree[i]==0:
+                dQ.append(i)
+
