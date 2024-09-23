e14ee86f12c00cccb0308276d6edffd0edd27bce
kimdongyoung <99941018+wwweric12@users.noreply.github.com>
2024-09-23 17:40:08 +0900

    [Gold III] Title: 레이저 통신, Time: 128 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.py
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.py
@@ -9,42 +9,39 @@ razer=[]
 
 dx=[0,1,0,-1]  # 왼 아래 0,1,2,3
 dy=[-1,0,1,0]
-ch=[[10000 for _ in range(w)] for _ in range(h)]
+ch = [[[10000 for _ in range(4)] for _ in range(w)] for _ in range(h)]
 
 for i in range(h):
     tmp =list(input().strip())
     for j in range(w):
         if tmp[j] == "C":
-            razer.append((i,j,-1,0))
+            razer.append((i,j))
     lst.append(tmp)
             
 Q=deque()
-Q.append(razer[0])
-ch[razer[0][0]][razer[0][1]]=0
+
+for i in range(4):
+    ch[razer[0][0]][razer[0][1]][i]=0
+    Q.append((razer[0][0],razer[0][1],i,0))
 
 while Q:
     x, y, direction,cnt = Q.popleft()
+  
     for i in range(4):
-        xx = x
-        yy = y
-        while True:
-            xx+=dx[i]
-            yy+=dy[i]
-            if not (0<= xx <h and 0<= yy <w) :
-                break
-            if lst[xx][yy]== "*":
-                break
-            if direction == -1 or direction == i:
-                if ch[xx][yy] > cnt:
-                    ch[xx][yy] = cnt
-                    Q.append((xx, yy, i, cnt))
-            else:
-                if ch[xx][yy] > cnt + 1:
-                    ch[xx][yy] = cnt + 1
-                    Q.append((xx, yy, i, cnt + 1))
+        xx = x+dx[i]
+        yy = y+dy[i]
+        if 0<= xx <h and 0<= yy <w and lst[xx][yy]!= "*":
+            if  direction == i :
+                if ch[xx][yy][i]>cnt  :
+                    ch[xx][yy][i]= cnt
+                    Q.appendleft((xx,yy,i,cnt))
+            elif  direction != i and  ch[xx][yy][i]>cnt+1 :
+                ch[xx][yy][i]= cnt+1
+                Q.append((xx,yy,i,cnt+1))
+    
 
 
 
-print(ch[razer[1][0]][razer[1][1]])
+print(min(ch[razer[1][0]][razer[1][1]]))
 
 
