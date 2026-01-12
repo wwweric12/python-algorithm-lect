250dbe6d183b446fa029004b61f29438816ba591
kimdongyoung <99941018+wwweric12@users.noreply.github.com>
2026-01-12 17:15:22 +0900

    [Gold III] Title: 개구리 점프, Time: 460 ms, Memory: 53432 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.py
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.py
@@ -1,6 +1,8 @@
 ## 좌표가 10의 9승 이하.. 
 
-
+import sys 
+sys.setrecursionlimit(10**6)
+input= sys.stdin.readline
 
 n, q = map(int,input().split())
 
@@ -9,26 +11,49 @@ lst=[]
 
 parents= [i for i in range(n+1)]
 
+
+def find_parents(node):
+    if node == parents[node]:
+        return node 
+    else:
+        parents[node] = find_parents(parents[node])
+        return parents[node]
+    
+def union(a,b):
+    parents_a = find_parents(a)
+    parents_b = find_parents(b)
+
+    if parents_a <  parents_b:
+        parents[parents_b]=parents_a     
+    else:
+        parents[parents_a] =parents_b
+
+
+
 for i in range(n):
     x1, x2 , y = map(int,input().split())
-    lst.append((x1,x2,y,i+1))
+    lst.append((x1,x2,i+1))
 
 lst.sort()
 
-for i in range(1,n):
-    px1, px2, py,pindex = lst[i-1]
-    x1, x2, y , index = lst[i]
 
-    if y == py:
-        continue
-    elif x1 <= px2 :
-    
-        parents[index] = parents[pindex]
+right_max=lst[0][1]
+parents_index=lst[0][2]
+
+for i in range(1,n):
+    x1,x2,index = lst[i]    
+    if right_max>= x1:
+        union(index,parents_index)
+        if right_max < x2:
+            right_max = x2
+    else:
+        right_max=x2
+        parents_index= index
 
 
 for i in range(q):
     a,b = map(int,input().split())
-    if parents[a] == parents[b]:
+    if find_parents(a) == find_parents(b):
         print(1)
     else:
         print(0)
