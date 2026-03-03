ba48a46328ad8e4791ef80ea7445a82772bdde96
kimdongyoung <99941018+wwweric12@users.noreply.github.com>
2026-03-03 12:21:17 +0900

    [Gold II] Title: 퍼즐, Time: 832 ms, Memory: 55428 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.py
+++ 백준/Gold/1525. 퍼즐/퍼즐.py
@@ -1,8 +1,8 @@
 
-from collections import defaultdict,deque
+from collections import deque
 lst=[]
 
-visited=defaultdict()
+visited=set()
 dy=[-1,0,0,1]
 dx=[0,1,-1,0]
 
@@ -10,54 +10,63 @@ dx=[0,1,-1,0]
 zeros=(0,0)
 
 for i in range(3):
-    tmp = list(input().split())
+    tmp = list(map(int,input().split()))
     for j in range(3):
-        if tmp[j] == "0":
+        if tmp[j] == 0:
             zeros =(i,j)
         lst.append(tmp[j])
 
 
 Q = deque([])
 
-Q.append((0,lst,zeros))
+binary=0
+
+for i in range(9):
+    binary |= (lst[i]<<(4*i))
+
+
+result=[1,2,3,4,5,6,7,8,0]
+result_value= 0
+for i in range(9):
+    result_value |= (result[i]<<(4*i))
+
+
+
+
+Q.append((0,binary,zeros))
 
 res=-1
 while Q:
-    cnt, array,c_zeros = Q.popleft()
-    y,x = c_zeros
-    array_str = "".join(array)
-    if array_str == "123456780":
+    cnt, binary_value,c_zeros = Q.popleft()
+    y,x = c_zeros 
+    if binary_value == result_value:
         res= cnt
         break
-    if array_str in visited:
+    if binary_value in visited:
         continue
-    visited[array_str]=1
+    visited.add(binary_value)
     for i in range(4):
         xx= x+dx[i]
         yy= y+dy[i]
-        tmp_array = array[:]
+        tmp_value =binary_value
         if 0<= xx < 3 and 0<= yy <3 :
-            tmp_array[x+y*3], tmp_array[xx+yy*3] = tmp_array[xx+yy*3], tmp_array[x+y*3]
-            tmp_array_str = "".join(tmp_array)
-            if tmp_array_str in visited:
+            tmp = (tmp_value>>(xx+3*yy)*4)&0xF
+            tmp_value &=~(0xF <<(xx+3*yy)*4)
+            tmp_value |= tmp <<((x+3*y)*4)
+
+            if tmp_value in visited:
                 continue
-            if tmp_array_str =="123456780":
+            if tmp_value ==result_value:
                 res= cnt+1
                 break
 
-            Q.append((cnt+1,tmp_array,(yy,xx)))
+            Q.append((cnt+1,tmp_value,(yy,xx)))
     if res != -1:
         break
 
 
 
 print(res)
-
-
-
-
-
-
 
 
 
